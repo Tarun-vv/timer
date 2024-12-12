c433bb67ccc82f71a12323b3519810ecbb4fcdb1
Tarun-vv <tarunv1911@gmail.com>
2024-12-11 20:34:11 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// const timerEl = document.querySelector(".timer");
+// console.log("hello!");
+
+// const startLogOutTimer = () => {
+//   // NOTE: Initialize time
+//   let time = 10;
+//   const tick = function () {
+//     // NOTE: get min and sec from time
+//     const min = String(Math.trunc(time / 60)).padStart(2, "0");
+//     const sec = String(time % 60).padStart(2, 0);
+
+//     // NOTE: set the content of timer div to the current time value
+//     timerEl.textContent = `${min}:${sec}`;
+
+//     // NOTE: handling after 0 sec
+//     if (time === 0) {
+//       clearInterval(timer);
+//     }
+
+//     // NOTE: decrease one second
+//     time--;
+
+//     console.log(time);
+//   };
+
+//   // NOTE: call time every second
+//   tick();
+//   const timer = setInterval(tick, 1000);
+// };
+
+// // startLogOutTimer();
+
+const timerEl = document.querySelector(".timer");
+console.log("hello!");
+
+const startLogOutTimer = () => {
+  // NOTE: Initialize time
+  let time = 10;
+
+  // NOTE: call time every second
+  const timer = setInterval(function () {
+    // NOTE: get min and sec from time
+    const min = String(Math.trunc(time / 60)).padStart(2, "0");
+    const sec = String(time % 60).padStart(2, 0);
+
+    // NOTE: set the content of timer div to the current time value
+    timerEl.textContent = `${min}:${sec}`;
+
+    // NOTE: handling after 0 sec
+    if (time === 0) {
+      clearInterval(timer);
+    }
+
+    // NOTE: decrease one second
+    time--;
+
+    console.log(time);
+  }, 1000);
+};
+
+startLogOutTimer();
---
+++ timer-v2.js
@@ -0,0 +1,29 @@
+const timerEl = document.querySelector(".timer");
+console.log("hello!");
+
+const startLogOutTimer = () => {
+  // NOTE: Initialize time
+  let time = 10;
+
+  // NOTE: call time every second
+  const timer = setInterval(function () {
+    // NOTE: get min and sec from time
+    const min = String(Math.trunc(time / 60)).padStart(2, "0");
+    const sec = String(time % 60).padStart(2, 0);
+
+    // NOTE: set the content of timer div to the current time value
+    timerEl.textContent = `${min}:${sec}`;
+
+    // NOTE: handling after 0 sec
+    if (time === 0) {
+      clearInterval(timer);
+    }
+
+    // NOTE: decrease one second
+    time--;
+
+    console.log(time);
+  }, 1000);
+};
+
+startLogOutTimer();
